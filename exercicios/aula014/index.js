aba5bbd6834244c9f7a6ec803efffbf3f8d79114
rafaelnevesdev <157032597+rafaelnevesdev@users.noreply.github.com>
2024-10-21 22:46:34 -0300

    Create index.js

---
+++ exercicios/aula014/index.js
@@ -0,0 +1,12 @@
+// IEE 754-2008
+let num1 = 0.7;
+let num2 = 0.1;
+
+console.log();
+
+//console.log(Number.isNaN(temp))
+// console.log(num1.toString() + num2)
+// num1 = num1.toString();
+// console.log(num1.toString(2));
+// console.log(num1.toFixed(2))
+// console.log(Number.isInteger(num1))
