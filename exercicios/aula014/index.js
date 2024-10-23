879284a2447aa80f1c382df8cc0edc027a3b2a7d
rafaelnevesdev <157032597+rafaelnevesdev@users.noreply.github.com>
2024-10-22 23:25:32 -0300

    Update index.js

--- exercicios/aula014/index.js
+++ exercicios/aula014/index.js
@@ -1,8 +1,25 @@
-// IEE 754-2008
+// IEE 754-2008 - Nome do Padrão do JS
 let num1 = 0.7;
 let num2 = 0.1;
 
-console.log();
+num1 += num2; // 0.8
+num1 += num2; // 0.9
+num1 += num2; // 1.0
+num1 += num2; // 1.1
+num1 += num2; // 1.2
+num1 += num2; // 1.3
+num1 += num2; // 1.4
+num1 += num2; // 1.5
+num1 += num2; // 1.6
+num1 += num2; // 1.7
+num1 += num2; // 1.8
+num1 += num2; // 1.9
+num1 += num2; // 2.0
+
+num1 = Number(num1.toFixed(2));
+
+console.log(Number.isInteger(num1));
+console.log(num1)
 
 //console.log(Number.isNaN(temp))
 // console.log(num1.toString() + num2)
