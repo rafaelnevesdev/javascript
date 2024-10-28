4277a1edb2fbef1aa60227c7a1280ec74f2982a1
rafaelnevesdev <157032597+rafaelnevesdev@users.noreply.github.com>
2024-10-27 23:54:53 -0300

    criação dos exercicios

---
+++ exercicios/aula015/ex/js/script.js
@@ -0,0 +1,13 @@
+const numero = Number(prompt('digite um numero: '));
+const numeroTitulo = document.getElementById('numero-tituloId');
+const texto = document.getElementById('textoId');
+
+
+numeroTitulo.innerHTML = numero;
+texto.innerHTML = '';
+texto.innerHTML += `<p>Raiz quadrada: ${numero ** 0.5}</p>`;
+texto.innerHTML += `<p>${numero} é inteiro: ${Number.isInteger(numero)}</p>`;
+texto.innerHTML += `<p>É NaN: ${Number.isNaN(numero)}</p>`;
+texto.innerHTML += `<p>Arredondado para baixo: ${Math.floor(numero)}</p>`;
+texto.innerHTML += `<p>Arredondado para cima: ${Math.ceil(numero)}</p>`;
+texto.innerHTML += `<p>Com duas casas decimais: ${numero.toFixed(2)}</p>`;
--- exercicios/aula015/index.js
+++ exercicios/aula015/index.js
@@ -1,6 +1,10 @@
 let num1 = 9.4
 
+console.log(maiorNumero)
 
+// let maiorNumero = Math.max(15, 50, 754, 1500, 80)
+// let menorNumero = Math.min(-5, 50, -100, 80, 2000)
+// const aleatorio2 = Math.round(Math.random() * (10 - 5) + 5);
 //let num2 = Math.round(num1);
 //num2 = Math.floor(num1);
 //num2 = Math.ceil(num1);
