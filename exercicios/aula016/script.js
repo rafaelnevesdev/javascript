946f05c13ff126d043fe1a2c6a6aca07047fc946
rafaelnevesdev <157032597+rafaelnevesdev@users.noreply.github.com>
2024-10-28 23:03:13 -0300

    Update script.js

--- exercicios/aula016/script.js
+++ exercicios/aula016/script.js
@@ -1,8 +1,14 @@
 const alunos = ['Rafael', 'Maria', 'João'];
 
-alunos[0] = 'Lucas';
-alunos[3] = 'Rayanne';
+alunos.unshift('Rayanne')
+alunos.unshift('Lucas')
+console.log(alunos);
 
-console.log(alunos)
-console.log(alunos[0]);
-console.log(alunos[3]);
+
+// alunos[alunos.length] = 'Jose';
+// alunos[alunos.length] = 'Rayanne';
+// alunos[alunos.length] = 'Lucas';
+//console.log(alunos[0]);
+//console.log(alunos[3]);
+// alunos[0] = 'Lucas';
+// alunos[3] = 'Rayanne';
