69fbed6ceb65bfde404e2ef2a9b25a940de8769c
rafaelnevesdev <157032597+rafaelnevesdev@users.noreply.github.com>
2024-11-16 16:10:46 -0300

    update aula017

---
+++ exercicios/aula017/script.js
@@ -0,0 +1,6 @@
+function saudacao(nome) {
+    console.log(`Bom dia ${nome}!`);
+}
+
+const variavel = saudacao('rafael');
+console.log(variavel)
---
+++ teste.js
@@ -0,0 +1,9 @@
+const alunos = ['Rafael', 2, true, teste]
+
+function teste(x, y) {
+
+    
+
+}
+
+console.log(alunos[1])
