315601cedbe4cf12ac32224b568ef3ed0576c1f5
rafaelneves <dev.rafaellucas@gmail.com>
2024-11-17 22:01:39 -0300

    Update aula019

---
+++ exercicios/aula019/script.js
@@ -0,0 +1,24 @@
+/*
+Primitivos (imutáveis) - string, number, boolean, undefined, null (bigint, symbol) - Valores copiados
+
+Referência (mutável) - array, object, function - Passados por referência
+*/
+
+let a = [1, 2, 3];
+let b = a;
+
+console.log(a, b);
+
+a.push(4);
+console.log(a, b);
+
+b.pop();
+console.log(a, b);
+
+
+/*let a = 'A';
+let b = a; // Cópia
+console.log(a, b);
+
+a = 'Outra coisa';
+console.log(a, b);*/
