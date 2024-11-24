d14edf8523f657fd24de23e5fc59ef8cf2c41e0b
Rafael Neves <dev.rafaellucas@gmail.com>
2024-11-24 15:16:54 -0300

    Finish aula020

--- exercicios/aula020/js/script.js
+++ exercicios/aula020/js/script.js
@@ -1,5 +1,6 @@
-function meuEscopo () {
+function meuEscopo() {
     const form = document.querySelector('.form');
+    const resultado = document.querySelector('.resultado');
     
     // form.onsubmit = function (evento) {
     //     evento.preventDefault();
@@ -9,7 +10,7 @@ function meuEscopo () {
 
     const pessoas = [];
 
-    function recebeEventoForm (evento) {
+    function recebeEventoForm(evento) {
         evento.preventDefault();
 
         const nome = form.querySelector('.nome');
@@ -17,10 +18,19 @@ function meuEscopo () {
         const peso = form.querySelector('.peso');
         const altura = form.querySelector('.altura');
 
+        pessoas.push({
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value
+        });
 
-
+        console.log(pessoas);
+        
+        resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} ${peso.value} ${altura.value}</p>`;
     }
 
     form.addEventListener('submit', recebeEventoForm);
+
 };
 meuEscopo();
