d01e760bad845dcf3a27c34d3731b2101d96cceb
Rafael Neves <dev.rafaellucas@gmail.com>
2024-11-24 18:15:21 -0300

    Finish aula201

---
+++ exercicios/aula021/index.js
@@ -0,0 +1,16 @@
+/*
+Operadores de comparação
+> maior que
+>= maior que ou igual a
+< menor que
+<= menor que ou igual a
+== igualdade (valor)
+=== igualdade estrita (valor e tipo)
+!= diferente (valor)
+!== diferente estrito (valor e tipo)
+*/
+
+const num1 = 15;
+const num2 = 20;
+const comp = 15 !== 20;
+console.log(comp);
