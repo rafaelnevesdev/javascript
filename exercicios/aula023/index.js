fa96d9b7dffe8db101d2275d0336ea218bd30799
Rafael Neves <dev.rafaellucas@gmail.com>
2024-12-01 22:19:58 -0300

    aula 023

---
+++ exercicios/aula023/index.js
@@ -0,0 +1,16 @@
+/*
+&& -> false && true -> false "o valor mesmo"
+|| -> true || false -> vai retornar "o valor verdadeiro"
+
+FALSY
+false
+0
+'' "" ``
+null / undefined
+NaN
+*/
+
+const corUsuario = null;
+const corPadrao = corUsuario || 'preto';
+
+console.log(corPadrao);
