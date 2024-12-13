92b68a9a6d6afe4bb06a04657dc3ec605a3bbe1c
Rafael Neves <dev.rafaellucas@gmail.com>
2024-12-12 23:42:03 -0300

    new features, but don't complete

---
+++ exercicios/aula025/assets/js/main.js
@@ -0,0 +1,34 @@
+function meuEscopo() {
+
+    const form = document.querySelector('.form');
+    const resultado = document.querySelector('.resultado');
+
+    function recebeEventoForm(evento) {
+        evento.preventDefault();
+
+
+        // variaveis
+        const pesoInput = Number(form.querySelector('.peso').value);
+        const alturaInput = Number(form.querySelector('.altura').value);
+
+        const peso = Number(pesoInput);
+        const altura = Number(alturaInput);
+
+        // validação valores numericos
+        if (isNaN(peso) || isNaN(altura)) {
+            console.error('Por favor, insira valores numéricos válidos para peso e altura.')
+        } else {
+            const imc = peso/(altura ** 2)
+        }
+
+        // fluxo de verificação
+        if (imc > 18.5) {
+            
+        }
+
+    }
+
+    form.addEventListener('submit',recebeEventoForm);
+
+}
+meuEscopo();
