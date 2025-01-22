35dc43976a17bcd7f02762c58d1e939aad454ba1
Rafael Neves <dev.rafaellucas@gmail.com>
2025-01-22 00:06:47 -0300

    edit excercise 028

--- exercicios/aula028/index.js
+++ exercicios/aula028/index.js
@@ -3,6 +3,40 @@ const diaSemana = data.getDay();
 
 let diaSemanaTexto;
 
+switch (diaSemana) {
+    case 0:
+        diaSemanaTexto = 'Domingo'
+        break;
+
+    case 1:
+        diaSemanaTexto = 'Segunda'
+        break;
+
+    case 2:
+        diaSemanaTexto = 'Terça'
+        break;
+    
+    case 3:
+        diaSemanaTexto = 'Quarta'
+        break;
+
+    case 4:
+        diaSemanaTexto = 'Quinta'
+        break;
+
+    case 5:
+        diaSemanaTexto = 'Sexta'
+        break;
+
+    case 6:
+        diaSemanaTexto = 'Sábado'
+        break;
+    
+    default:
+        diaSemanaTexto = 'Valor invalido'
+        break;
+}
+
 // if (diaSemana === 0) {
 //     diaSemanaTexto = 'Domingo';
 // } else if (diaSemana === 1) {
@@ -17,6 +51,8 @@ let diaSemanaTexto;
 //     diaSemanaTexto = 'Sexta';
 // } else if (diaSemana === 6) {
 //     diaSemanaTexto = 'Sábado';
+// } else {
+//     diaSemanaTexto = 'Valor invalido';
 // }
 
 
