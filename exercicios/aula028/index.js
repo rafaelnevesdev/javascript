7738f7c37e8cc73fe9ae577bf2fec04f90f68e5e
Rafael Neves <dev.rafaellucas@gmail.com>
2025-01-23 21:47:09 -0300

    exercise finished

--- exercicios/aula028/index.js
+++ exercicios/aula028/index.js
@@ -37,6 +37,52 @@ switch (diaSemana) {
         break;
 }
 
+// em funções não precisamos por o break
+
+/*
+function getDiaSemanaTexto(diaSemana) {
+    
+    
+    let diaSemanaTexto;
+
+
+    switch (diaSemana) {
+        case 0:
+            diaSemanaTexto = 'Domingo'
+            return diaSemanaTexto;
+    
+        case 1:
+            diaSemanaTexto = 'Segunda'
+            return diaSemanaTexto;
+    
+        case 2:
+            diaSemanaTexto = 'Terça'
+            return diaSemanaTexto;
+        
+        case 3:
+            diaSemanaTexto = 'Quarta'
+            return diaSemanaTexto;
+    
+        case 4:
+            diaSemanaTexto = 'Quinta'
+            return diaSemanaTexto;
+    
+        case 5:
+            diaSemanaTexto = 'Sexta'
+            return diaSemanaTexto;
+    
+        case 6:
+            diaSemanaTexto = 'Sábado'
+            return diaSemanaTexto;
+        
+        default:
+            diaSemanaTexto = 'Valor invalido'
+            return diaSemanaTexto;
+    }
+    
+}
+*/
+
 // if (diaSemana === 0) {
 //     diaSemanaTexto = 'Domingo';
 // } else if (diaSemana === 1) {
