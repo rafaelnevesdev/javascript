929fb573e9b6cb38711115a3f5b319aa9cc12bb4
Rafael Neves <dev.rafaellucas@gmail.com>
2025-01-26 23:38:14 -0300

    exercise 030 finished

---
+++ exercicios/aula031/index.js
@@ -0,0 +1,9 @@
+// const numeros = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000];
+// const [um, , tres, , cinco, , sete] = numeros;
+// console.log(um, tres, cinco, sete);
+
+const numeros = [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ];
+const [lista1, lista2, lista3] = numeros;
+
+console.log(lista3[2]);
+
