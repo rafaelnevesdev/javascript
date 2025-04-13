2ef3598ea4038eef58dec18165e21ef495ed4b57
Rafael Neves <dev.rafaellucas@gmail.com>
2025-04-13 14:54:11 -0300

    aula35 - for in

--- exercicios/aula035/main.js
+++ exercicios/aula035/main.js
@@ -1,9 +1,22 @@
-const frutas = ['Pera', 'Maçã', 'Uva'];
+const pessoa = {
+    nome: 'Rafael',
+    sobrenome: 'Neves',
+    idade: 30
+};
 
+for (let key in pessoa) {
+    console.log(key, pessoa[key]);
+}
+
+// const frutas = ['Pera', 'Maçã', 'Uva'];
+
+// forma manual (sem for in)
 // for (let i = 0; i < frutas.length; i++) {
 //     console.log(frutas[i])
 // }
 
-for (let i in frutas) {
-    console.log(i);
-}
+
+// forma adequada para arrays e objetos (com for in)
+// for (let index in frutas) {
+//     console.log(index);
+// }
