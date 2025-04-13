e775e26cef03c93d1377bb1049968df6d41592fb
Rafael Neves <dev.rafaellucas@gmail.com>
2025-04-13 16:19:14 -0300

    aula36 - for of e forEach

---
+++ exercicios/aula036/main.js
@@ -0,0 +1,30 @@
+const pessoa = {
+    nome: 'Rafael',
+    sobrenome: 'Neves',
+    idade: 30
+};
+
+// const nome = ['Rafael', 'Lucas', 'Santos', 'Neves'];
+
+// for (let valor of nome) {
+//     console.log(valor);
+// }
+
+// console.log('###########');
+
+// for (let valor in nome) {
+//     console.log(nome[valor]);
+// }
+
+// nome.forEach(function(valor, indice, array) {
+//     console.log(valor, indice, array);
+    
+// });
+
+// for (let i in nome) {
+//     console.log(nome[i]);
+// }
+
+// for (let i = 0; i < nome.length; i++) {
+//     console.log(nome[i]);
+// }
