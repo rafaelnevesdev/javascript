8afded8554a4dcd4b5f58cbb5dd666dd59206d77
Rafael Neves <rafaelneves.softwaredeveloper@gmail.com>
2025-08-28 12:15:27 -0300

    aula041 - função com ternarios passando true ou false

---
+++ exercicios/aula041/main.js
@@ -0,0 +1,6 @@
+// Escreva uma função chamada ePaisagem que recebe dois argumentos, largura e altura de uma imagem (Number). Retorne true se a imagem estiver no modo paisagem.
+
+const ePaisagem = (width, heigth) => width >= heigth;
+
+
+console.log(ePaisagem(49, 50));
