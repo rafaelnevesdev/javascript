6fbdbd4aa061d824824934438e9b21b2862d794b
rafaelnevesdev <rafaelneves.softwaredeveloper@gmail.com>
2026-08-18 00:14:21 -0300

    Aula 049: Em andamento.

---
+++ exercicios/aula049/script.js
@@ -0,0 +1,9 @@
+// arguments sustenta todos os argumentos enviados.
+function funcao(a, b, c) {
+    let total = 0;
+    for (let argumento of arguments) {
+        total += argumento;
+    }
+    console.log(total, a, b, c);
+}
+funcao(1, 2, 3, 4, 5, 6, 7);
